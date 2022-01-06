f580e8990665cc597c021f012a02275c918cd855
Wanding Zhou - Bioinformatics <zhouwanding@gmail.com>
2022-01-06 13:46:02 -0500

    Update 20220105_Infinium_mapping_merge.R

--- Snippets/20220105_Infinium_mapping_merge.R
+++ Snippets/20220105_Infinium_mapping_merge.R
@@ -33,10 +33,10 @@ mergeI_and_II_1 <- function(x) {
     Probe_ID = probeID,
     lastBase_A = lastA, mapFlag_A = flagA, mapChrm_A = samChrmA, mapPos_A = samPosA, mapQ_A = mapqA, 
     mapCigar_A = cigarA, AlleleA_ProbeSeq = p2seqA[probeID],
-    mapNM_A = nmA, mapAS_A = asA, mapYD_A = ydA, 
+    mapNM_A = as.integer(str_replace(nmA, "NM:i:","")), mapAS_A = as.integer(str_replace(asA, "AS:i:","")), mapYD_A = str_replace(ydA, "YD:A:",""), 
     lastBase_B = lastB, mapFlag_B = flagB, mapChrm_B = samChrmB, mapPos_B = samPosB, mapQ_B = mapqB, 
     mapCigar_B = cigarB, AlleleB_ProbeSeq = p2seqB[probeID],
-    mapNM_B = nmB, mapAS_B = asB, mapYD_B = ydB,
+    mapNM_B = as.integer(str_replace(nmB, "NM:i:","")), mapAS_B = as.integer(str_replace(asB, "AS:i:","")), mapYD_B = str_replace(ydB, "YD:A:",""), 
     design = NA,
     mask = NA, type = type))
   saveRDS(df4, file=sprintf('mapping/%s.rds', x))
